b30a746f12632a83be84c06e65f5971a8abec081
SvDovgun <sv.dovgun@gmail.com>
2022-09-04 23:25:54 +0300

    Added more

--- src/main/java/com/luxoft/practices/leetcode/array/MaxProfit.java
+++ src/main/java/com/luxoft/practices/leetcode/array/MaxProfit.java
@@ -1,2 +1,31 @@
-package com.luxoft.practices.leetcode.array;public class MaxProfit {
+package com.luxoft.practices.leetcode.array;
+
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        int[] prices = {6,1,3,2,4,7};
+
+        print(prices);
+        System.out.println(maxProfit(prices));
+
+    }
+
+    private static void print(int[] prices) {
+        for (int price: prices ) {
+            System.out.print(price + ", ");
+        }
+    }
+
+    private static int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            System.out.println("step  " + i);
+            if (prices[i] > prices[i - 1]) {
+                System.out.println(prices[i] + " -  " + prices[i - 1] + " = " + (prices[i] - prices[i - 1]));
+                maxprofit += prices[i] - prices[i - 1];
+                System.out.println("maxprofit =" + maxprofit);
+            }
+        }
+        return maxprofit;
+    }
 }
--- src/main/java/com/luxoft/practices/leetcode/array/RemoveDuplicates.java
+++ src/main/java/com/luxoft/practices/leetcode/array/RemoveDuplicates.java
@@ -1,2 +1,38 @@
-package com.luxoft.practices.leetcode.array;public class RemoveDuplicates {
+package com.luxoft.practices.leetcode.array;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,3,3,5,5,6,6,7,9,9};
+
+        print(nums);
+        System.out.println("  ");
+        System.out.println("Amount of unique numbers is " + removeDuplicates(nums));
+        print(nums);
+    }
+
+    private static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        for (int n = i +1; n < nums.length; n++) {
+            nums[n] = 0;
+        }
+        return i + 1;
+    }
+
+    private static void print(int[] nums) {
+        for (int i = 0; i < nums.length ; i++) {
+            if (i < nums.length - 1) {
+                System.out.print(nums[i] + ", ");
+            } else {
+                System.out.println(nums[i]);
+            }
+        }
+    }
 }
